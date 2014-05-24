5f4adc2af19c3c195865c247927dd7b19e285e0a
vpv <proggergood@gmail.com>
2014-05-25 02:59:33 +0700

    Added RewriteBase

--- sitebuilder/themes/bootstrap/views/layouts/main.php
+++ sitebuilder/themes/bootstrap/views/layouts/main.php
@@ -75,7 +75,7 @@
     <div class="clear"></div>
 
     <div id="footer">
-        Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+        Copyright &copy; <?php echo date('Y'); ?> by <a href="mailto:proggergood@gmail.com">ProggerGood</a>.<br/>
         All Rights Reserved.<br/>
         <?php echo Yii::powered(); ?>
     </div><!-- footer -->
