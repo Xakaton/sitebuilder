1d10ffab0f198a92830edcd2fdb4e392e1ab637d
vpv <proggergood@gmail.com>
2014-05-25 04:22:26 +0700

    Added redirects to areas

--- sitebuilder/protected/controllers/SiteController.php
+++ sitebuilder/protected/controllers/SiteController.php
@@ -99,6 +99,11 @@ class SiteController extends Controller
 			$model->attributes=$_POST['LoginForm'];
 			// validate user input and redirect to the previous page if valid
 			if($model->validate() && $model->login())
+                if (Yii::app()->user->role == 'admin') {
+                    $this->redirect('/admin');
+                } else if (Yii::app()->user->role == 'user') {
+                    $this->redirect('/factory');
+                }
 				$this->redirect(Yii::app()->user->returnUrl);
 		}
 		// display the login form
