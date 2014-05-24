cd9cf40e9dd4fc24ed5f293ed07ed8a77f767670
vpv <proggergood@gmail.com>
2014-05-25 00:11:52 +0700

    Init bootstrap theme in module factory

--- sitebuilder/protected/modules/factory/controllers/DefaultController.php
+++ sitebuilder/protected/modules/factory/controllers/DefaultController.php
@@ -2,6 +2,12 @@
 
 class DefaultController extends Controller
 {
+    public function init()
+    {
+        Yii::app()->theme = 'bootstrap';
+        parent::init();
+    }
+
 	public function actionIndex()
 	{
 		$this->render('index');
